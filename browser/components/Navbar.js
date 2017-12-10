a25ddec18f6e94df113a49a1e6623bd5909f1c17
“Sam <sam.alsmadi@gmail.com>
2017-12-10 16:38:41 -0500

    Rename some of the css class names for consistency

--- browser/components/Navbar.js
+++ browser/components/Navbar.js
@@ -7,8 +7,8 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav>
-        <div className="school-header">
-          <div className="school-header-buttons">
+        <div className="campus-header">
+          <div className="campus-header-buttons">
                 <h2>Margaret Hamilton Interplanetary Academy of JavaScript</h2>
                 <Button.Group>
                   <Link to="/campuses"><Button>Home</Button></Link>
@@ -16,7 +16,7 @@ export default class Navbar extends Component {
                   <Link to="/students"><Button positive>Students</Button></Link>
                 </Button.Group>
             </div>
-            <div className="school-header-logo">
+          <div className="campus-header-logo">
                 <Icon.Group size='big'>
                   <Icon loading size='huge' name='sun' />
                   <Icon name='student' />
@@ -24,46 +24,7 @@ export default class Navbar extends Component {
             </div>
           </div>
           <hr />
-        {/* <Icon name='student' size='massive' /> */}
-          {/* <Header
-            as='h1'
-            image='/assets/images/icons/school.png'
-
-            content='Margaret Hamilton Interplanetary Academy of JavaScript'
-          /> */}
-{/*
-
-  <Menu>
-            <Menu.Item>
-                <Icon.Group size='large'>
-              <Icon loading size='big' name='sun' />
-              <Icon name='student' />
-            </Icon.Group>
-                    </Menu.Item>
-        <Menu.Item>
-            <h2>Margaret Hamilton Interplanetary Academy of JavaScript</h2>
-    </Menu.Item>
-            <Menu.Item>
-             <Button.Group>
-              <Link to="/campuses"><Button>Home</Button></Link>
-              <Button.Or />
-              <Link to="/students"><Button positive>Students</Button></Link>
-            </Button.Group>
-                    </Menu.Item>
-
-
-    {/* <Menu.Item>
-      <Link to="/campuses"><Button primary>Campuses</Button></Link>
-
-    </Menu.Item>
-
-    <Menu.Item>
-      <Link to="/students"><Button>Students</Button></Link>
-    </Menu.Item>  </Menu>*/}
-
       </nav>
     );
   }
 }
-
-
